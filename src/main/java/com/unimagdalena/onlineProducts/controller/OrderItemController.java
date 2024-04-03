cf9add205cd69d4fde528619544130ab81fd13ee
diegoballesteros <diegoballesteros777@gmail.com>
2024-04-03 01:37:41 -0500

    agregando controladores finales

--- src/main/java/com/unimagdalena/onlineProducts/controller/OrderItemController.java
+++ src/main/java/com/unimagdalena/onlineProducts/controller/OrderItemController.java
@@ -1,9 +1,70 @@
 package com.unimagdalena.onlineProducts.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.unimagdalena.onlineProducts.persistence.DTO.OrderItemDto;
+import com.unimagdalena.onlineProducts.persistence.entity.OrderItemEntity;
+import com.unimagdalena.onlineProducts.persistence.mapper.OrderItemMapper;
+import com.unimagdalena.onlineProducts.service.OrderItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/order-items")
 public class OrderItemController {
+    private final OrderItemService orderItemService;
+    private final OrderItemMapper orderItemMapper;
+
+    public OrderItemController(OrderItemService orderItemService, OrderItemMapper orderItemMapper) {
+        this.orderItemService = orderItemService;
+        this.orderItemMapper = orderItemMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderItemDto>> getAll(){
+        return ResponseEntity.ok(this.orderItemService.getAll());
+    }
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<List<OrderItemDto>> findAllByOrderId(@PathVariable int orderId){
+        return ResponseEntity.ok(this.orderItemService.findAllByOrderId(orderId));
+    }
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<List<OrderItemDto>> findAllByProductId(@PathVariable int productId){
+        return ResponseEntity.ok(this.orderItemService.findAllByProductId(productId));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderItemDto> getByid(@PathVariable int id){
+        return ResponseEntity.ok(this.orderItemService.findByIdOrderItem(id));
+    }
+    @PostMapping()
+    public ResponseEntity<OrderItemDto> addOrderItem (@RequestBody OrderItemDto orderItemDto){
+        OrderItemEntity orderItemEntity = this.orderItemMapper.orderItemDtoToOrderItemEntity(orderItemDto);
+        if(orderItemEntity.getIdOrderItem()==null || !this.orderItemService.existsByIdOrderItem(Math.toIntExact(orderItemEntity.getIdOrderItem()))){
+            OrderItemEntity orderItemCreated = this.orderItemService.save(orderItemEntity);
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{idOrder}")
+                    .buildAndExpand(orderItemCreated.getIdOrderItem())
+                    .toUri();
+            OrderItemDto orderItemDto1 = this.orderItemMapper.orderItemEntityToOrderItemDto(orderItemCreated);
+            return  ResponseEntity.created(location).body(orderItemDto1);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<OrderItemEntity> update(@PathVariable int id, @RequestBody OrderItemDto orderItemDto){
+        OrderItemEntity orderItem = this.orderItemService.update(id,orderItemDto);
+        return ResponseEntity.ok(orderItem);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id){
+        if(this.orderItemService.existsByIdOrderItem(id)){
+            this.orderItemService.delete(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
 }
--- src/main/java/com/unimagdalena/onlineProducts/controller/ShipmentDetailController.java
+++ src/main/java/com/unimagdalena/onlineProducts/controller/ShipmentDetailController.java
@@ -1,10 +1,65 @@
 package com.unimagdalena.onlineProducts.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.unimagdalena.onlineProducts.persistence.DTO.ShipmentDetailDto;
+import com.unimagdalena.onlineProducts.persistence.entity.ShipmentDetailEntity;
+import com.unimagdalena.onlineProducts.persistence.mapper.ShipmentDetailMapper;
+import com.unimagdalena.onlineProducts.service.ShipmentDetailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/shipping")
 public class ShipmentDetailController {
-    
+    private final ShipmentDetailService shipmentDetailService;
+    private final ShipmentDetailMapper shipmentDetailMapper;
+
+    public ShipmentDetailController(ShipmentDetailService shipmentDetailService, ShipmentDetailMapper shipmentDetailMapper) {
+        this.shipmentDetailService = shipmentDetailService;
+        this.shipmentDetailMapper = shipmentDetailMapper;
+    }
+    @GetMapping
+    public ResponseEntity<List<ShipmentDetailDto>> getAll(){
+        return ResponseEntity.ok(this.shipmentDetailService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ShipmentDetailDto> getByid(@PathVariable int id){
+        return ResponseEntity.ok(this.shipmentDetailService.findByIdDetail(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<ShipmentDetailDto> addShipment (@RequestBody ShipmentDetailDto shipmentDto){
+        ShipmentDetailEntity shipmentDetailEntity = this.shipmentDetailMapper.shipmentDetailDtoToShipmentDetailEntity(shipmentDto);
+        if(shipmentDetailEntity.getIdDetail()==null || !this.shipmentDetailService.existsByIdDetail(Math.toIntExact(shipmentDetailEntity.getIdDetail()))){
+            ShipmentDetailEntity shipmentCreated = this.shipmentDetailService.save(shipmentDetailEntity);
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{idOrder}")
+                    .buildAndExpand(shipmentCreated.getIdDetail())
+                    .toUri();
+            ShipmentDetailDto shipmentDto1 = this.shipmentDetailMapper.ShipmentDetailEntityToShipmentDetailDto(shipmentCreated);
+            return  ResponseEntity.created(location).body(shipmentDto1);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ShipmentDetailEntity> update(@PathVariable int id, @RequestBody ShipmentDetailDto shipmentDetailDto){
+        ShipmentDetailEntity shipping = this.shipmentDetailService.update(id,shipmentDetailDto);
+        return ResponseEntity.ok(shipping);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id){
+        if(this.shipmentDetailService.existsByIdDetail(id)){
+            this.shipmentDetailService.delete(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+        // falta end poind no crud
+    }
 }
